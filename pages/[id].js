2641c7ec73f8b5f305fdd830825db8dec43ab901
trevorthebike <trevorthebike@gmail.com>
2022-09-14 13:45:19 -0700

    2 json files

--- pages/[id].js
+++ pages/[id].js
@@ -1,26 +1,30 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout from '../components/layout';
-import { getAllIds, getData } from '../lib/data';
+import { getAllIds,getData} from '../lib/data';
 
 export async function getStaticProps({ params }) {
-  const itemData = await getData(params.id);
-  // console.log(itemData);
+  let jsontype = 0;
+  const itemData = await getData(params.id, jsontype);
+  const itemData1 = await getData(params.id, jsontype+1);
   return {
     props: {
-      itemData
+      itemData,
+      itemData1
     }
   };
 }
 
 export async function getStaticPaths() {
   const paths = getAllIds();
+  console.log(paths)
   return {
     paths,
     fallback: false
   };
 }
 
-export default function Entry({ itemData }) {
+export default function Entry({ itemData,itemData1 }) {
   return (
     <Layout>
       <article className="card col-6">
@@ -31,6 +35,14 @@ export default function Entry({ itemData }) {
           <a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a>
         </div>
       </article>
-    </Layout>
+    <article className="card col-6">
+      <div className="card-body">
+        <h5 className="card-title">{itemData1.name}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{itemData1.phone}</h6>
+        <p className="card-text">{itemData1.birthdate}</p>
+        <a href={'mailto:' + itemData.email} className="card-link">{itemData1.email}</a>
+      </div>
+    </article>
+  </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout';
-import { getSortedList } from '../lib/data';
+import { getSortedList} from '../lib/data';
 
 export async function getStaticProps() {
   const allData = getSortedList();
