8198accc2422d2cb8a288c49a7b58293a9f12f2d
trevorthebike <trevorthebike@gmail.com>
2022-09-21 21:12:18 -0700

    test final

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,15 +1,24 @@
 import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 import '@testing-library/jest-dom'
+import { Children } from 'react'
 
-describe('Home', () => {
-  it('renders a heading', () => {
+describe('Main', () => {
+  it('renders main', () => {
     render(<Home />)
-
-    const heading = screen.getByRole('heading', {
-      name: /CS55.13 Week 4 Trevor/i,
+    const main1 = screen.getByRole('link', {
+      name: /GitHub Link/i,
     })
-
-    expect(heading).toBeInTheDocument()
+    expect(main1).toBeInTheDocument()
+  
   })
 })
+
+describe('', () => {
+  it('renders text', () => {
+    render(<Home />)
+    const text1 = screen.getByText(/Sorted names with links/i)
+    expect(text1).toBeInTheDocument()
+  })
+})
+
