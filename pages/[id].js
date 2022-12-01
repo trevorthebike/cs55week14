390f4f6cec5b4d6724bc3e61656b8b64080b222a
trevorthebike <trevorthebike@gmail.com>
2022-11-30 18:35:50 -0800

    test1

--- pages/[id].js
+++ pages/[id].js
@@ -27,10 +27,11 @@ export default function Entry({ itemData }) {
     <Layout>
       <article className="card col-6">
         <div className="card-body">
+          <h1 className="card-title">{itemData.ID}</h1>
           <h4 className="card-title">{itemData.post_title}</h4>
-          <h4 className="card-title">{itemData.post_author}</h4>
-          <p className="card-subtitle">{itemData.post_date}</p>
+          <p className="card-subtitle">{itemData.post_status}</p>
           <div className = "card-text" dangerouslySetInnerHTML = {{__html: itemData.post_content}} />
+          <h2 className = "card-title">{itemData.new_fields} </h2>
         </div>
       </article>
   </Layout>
